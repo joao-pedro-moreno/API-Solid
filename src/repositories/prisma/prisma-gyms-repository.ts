b616feb08b2a3c297e13186d6ff620afd36feffd
João Pedro Moreno <devmoreno2003@gmail.com>
2024-03-06 15:34:19 -0300

    Updating Gyms Prisma repository

---
+++ src/repositories/prisma/prisma-gyms-repository.ts
@@ -0,0 +1,45 @@
+import { Gym, Prisma } from '@prisma/client'
+import { FindManyNearbyParams, GymsRepository } from '../gyms-repository'
+import { prisma } from '@/lib/prisma'
+
+export class PrismaGymsRepository implements GymsRepository {
+  async findById(id: string) {
+    const gym = await prisma.gym.findUnique({
+      where: {
+        id,
+      },
+    })
+
+    return gym
+  }
+
+  async searchMany(query: string, page: number) {
+    const gyms = await prisma.gym.findMany({
+      where: {
+        title: {
+          contains: query,
+        },
+      },
+      take: 20,
+      skip: (page - 1) * 20,
+    })
+
+    return gyms
+  }
+
+  async findManyNearby(params: FindManyNearbyParams) {
+    const gyms = await prisma.$queryRaw<Gym[]>`
+    SELECT * from gyms
+    WHERE ( 6371 * acos( cos( radians(${params.userLatitude}) ) * cos( radians( latitude ) ) * cos( radians( longitude ) - radians(${params.userLongitude}) ) + sin( radians(${params.userLatitude}) ) * sin( radians( latitude ) ) ) ) <= 10`
+
+    return gyms
+  }
+
+  async create(data: Prisma.GymCreateInput) {
+    const gym = await prisma.gym.create({
+      data,
+    })
+
+    return gym
+  }
+}
